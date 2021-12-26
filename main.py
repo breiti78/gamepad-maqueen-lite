a97d0431eef43ab445f3c00953571b123b993f3f
breiti78 <remo@breitis.ch>
2021-12-26 16:08:37 +0100

    LED Ring

--- main.py
+++ main.py
@@ -1,3 +1,57 @@
+def FollowLine():
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    elif maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    elif maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        else:
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+def recieveString(receievedString: str):
+    global lastButton
+    if receievedString == "A":
+        if receievedString != lastButton:
+            _36GradLinks()
+    elif receievedString == "B":
+        FollowLine()
+    elif receievedString == "P16":
+        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_ON)
+    elif receievedString == "P14":
+        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_ON)
+    elif receievedString.includes("FW"):
+        if hatHinderniss == 0:
+            maqueen.motor_run(maqueen.Motors.ALL,
+                maqueen.Dir.CW,
+                (parse_float(receievedString.substr(2, 10)) - 1) / 2)
+    elif receievedString.includes("BW"):
+        maqueen.motor_run(maqueen.Motors.ALL,
+            maqueen.Dir.CCW,
+            (parse_float(receievedString.substr(2, 10)) - 1) / 2)
+    elif receievedString == "L":
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+    elif receievedString == "R":
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    elif receievedString == "P15":
+        if receievedString != lastButton:
+            LEDFarbeÄndern()
+    else:
+        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
+        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
+        maqueen.motor_stop(maqueen.Motors.ALL)
+    lastButton = receievedString
 def LED_Init():
     global strip, LEDFarben, aktuelleFarbe
     strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
@@ -12,40 +66,8 @@ def LED_Init():
     aktuelleFarbe = 0
 
 def on_received_string(receivedString):
-    global lastButton
+    recieveString(receivedString)
     serial.write_line(receivedString)
-    if receivedString == "A":
-        if receivedString != lastButton:
-            _36GradLinks()
-    elif receivedString == "B":
-        pass
-    elif receivedString == "P16":
-        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_ON)
-    elif receivedString == "P14":
-        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_ON)
-    elif receivedString.includes("FW"):
-        if hatHinderniss == 0:
-            maqueen.motor_run(maqueen.Motors.ALL,
-                maqueen.Dir.CW,
-                (parse_float(receivedString.substr(2, 10)) - 1) / 2)
-    elif receivedString.includes("BW"):
-        maqueen.motor_run(maqueen.Motors.ALL,
-            maqueen.Dir.CCW,
-            (parse_float(receivedString.substr(2, 10)) - 1) / 2)
-    elif receivedString == "L":
-        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 100)
-    elif receivedString == "R":
-        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 100)
-        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 20)
-    elif receivedString == "P15":
-        if receivedString != lastButton:
-            LEDFarbeÄndern()
-    else:
-        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
-        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
-        maqueen.motor_stop(maqueen.Motors.ALL)
-    lastButton = receivedString
 radio.on_received_string(on_received_string)
 
 def LEDFarbeÄndern():
@@ -78,14 +100,27 @@ def _36GradLinks():
         maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
     maqueen.motor_stop(maqueen.Motors.ALL)
-hatHinderniss = 0
-lastButton = ""
 aktuelleFarbe = 0
 LEDFarben: List[str] = []
 strip: neopixel.Strip = None
+hatHinderniss = 0
+lastButton = ""
 radio.set_group(1)
 LED_Init()
-
+basic.show_leds("""
+    . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+""")
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
 
 def on_forever():
     global hatHinderniss
@@ -94,26 +129,4 @@ def on_forever():
         hatHinderniss = 1
     else:
         hatHinderniss = 0
-        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
-                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        else:
-                if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
-                    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-                    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-                    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
-                        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-                        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-                else:
-                    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
-                        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-                        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-                        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
-                            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-                            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-                        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
-                            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-                        else:
-                            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
 basic.forever(on_forever)
-
--- main.ts
+++ main.ts
@@ -23,55 +23,32 @@ function FollowLine () {
         }
     }
 }
-function _36GradLinks () {
-    for (let index = 0; index < 600; index++) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-    }
-    maqueen.motorStop(maqueen.Motors.All)
-}
-function LED_Init () {
-    strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-    LEDFarben = [
-    "rot",
-    "orange",
-    "gelb",
-    "grün",
-    "blau",
-    "indigo",
-    "blauviolett",
-    "magenta"
-    ]
-    aktuelleFarbe = 0
-}
-radio.onReceivedString(function (receivedString) {
-    serial.writeLine(receivedString)
-    if (receivedString == "A") {
-        if (receivedString != lastButton) {
+function recieveString (receievedString: string) {
+    serial.writeLine(receievedString)
+    if (receievedString == "A") {
+        if (receievedString != lastButton) {
             _36GradLinks()
         }
-    } else if (receivedString == "B") {
-        if (receivedString != lastButton) {
-            FollowLine()
-        }
-    } else if (receivedString == "P16") {
+    } else if (receievedString == "B") {
+        FollowLine()
+    } else if (receievedString == "P16") {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-    } else if (receivedString == "P14") {
+    } else if (receievedString == "P14") {
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    } else if (receivedString.includes("FW")) {
+    } else if (receievedString.includes("FW")) {
         if (hatHinderniss == 0) {
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, (parseFloat(receivedString.substr(2, 10)) - 1) / 2)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, (parseFloat(receievedString.substr(2, 10)) - 1) / 2)
         }
-    } else if (receivedString.includes("BW")) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, (parseFloat(receivedString.substr(2, 10)) - 1) / 2)
-    } else if (receivedString == "L") {
+    } else if (receievedString.includes("BW")) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, (parseFloat(receievedString.substr(2, 10)) - 1) / 2)
+    } else if (receievedString == "L") {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
-    } else if (receivedString == "R") {
+    } else if (receievedString == "R") {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
-    } else if (receivedString == "P15") {
-        if (receivedString != lastButton) {
+    } else if (receievedString == "P15") {
+        if (receievedString != lastButton) {
             LEDFarbeÄndern()
         }
     } else {
@@ -79,7 +56,38 @@ radio.onReceivedString(function (receivedString) {
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         maqueen.motorStop(maqueen.Motors.All)
     }
-    lastButton = receivedString
+    lastButton = receievedString
+}
+function fahrerückwärts () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, x - y)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, x + y)
+}
+function LED_Init () {
+    strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+    LEDRing = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
+    aktuelleFarbe = 0
+    LEDRing.showRainbow(1, 360)
+    LEDRing.show()
+}
+function fahrevorwärts () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, x - y)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, x + y)
+}
+radio.onReceivedValue(function (name, value) {
+    serial.writeValue(name, value)
+    if (name == "x") {
+        x = value
+    }
+    if (name == "y") {
+        y = value
+    }
+    if (y > 10) {
+        fahrevorwärts()
+    } else if (y < -10) {
+        fahrerückwärts()
+    } else {
+        Motor_Stop()
+    }
 })
 function LEDFarbeÄndern () {
     if (aktuelleFarbe == 7) {
@@ -108,13 +116,39 @@ function LEDFarbeÄndern () {
     }
     strip.show()
 }
+function _36GradLinks () {
+    for (let index = 0; index < 600; index++) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+    }
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function Motor_Stop () {
+    maqueen.motorStop(maqueen.Motors.All)
+}
+let aktuelleFarbe = 0
+let LEDRing: neopixel.Strip = null
+let strip: neopixel.Strip = null
+let y = 0
+let x = 0
 let hatHinderniss = 0
 let lastButton = ""
-let aktuelleFarbe = 0
-let LEDFarben: string[] = []
-let strip: neopixel.Strip = null
 radio.setGroup(1)
 LED_Init()
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) > 0 && maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
         maqueen.motorStop(maqueen.Motors.All)
